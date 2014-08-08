8d8804edbdce3c7baa17862544e0b535867c7b62
Boris T. Darchiev <boris.darchiev@gmail.com>
2014-08-08 16:03:49 +0400

    int types for windows w/o boost & C++11 #3

--- src/nkit/types.h
+++ src/nkit/types.h
@@ -72,14 +72,15 @@
 // pass
 #elif defined(NKIT_WINNT)
 
-  typedef __int8            int8_t;
-  typedef __int16           int16_t;
-  typedef __int32           int32_t;
-  typedef __int64           int64_t;
-  typedef unsigned __int8   uint8_t;
-  typedef unsigned __int16  uint16_t;
-  typedef unsigned __int32  uint32_t;
-  typedef unsigned __int64  uint64_t;
+#  include <stdint.h>
+//  typedef __int8            int8_t;
+//  typedef __int16           int16_t;
+//  typedef __int32           int32_t;
+//  typedef __int64           int64_t;
+//  typedef unsigned __int8   uint8_t;
+//  typedef unsigned __int16  uint16_t;
+//  typedef unsigned __int32  uint32_t;
+//  typedef unsigned __int64  uint64_t;
 
 #endif
 
